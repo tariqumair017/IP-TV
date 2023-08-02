93aad712fd5d8889dea087a52894fe32594557ab
M-Umair <tariqumair017@gmail.com>
2023-08-02 18:19:59 +0500

    All User Apis, Some Stream Apis, Some Episode Apis and Some genre Apis are complete

--- src/models/season.model.js
+++ src/models/season.model.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 const schema = Schema({
-  episodes: { type: Schema.Types.ObjectId, required: true, ref: 'Episode' },
+  episodes: [{ type: Schema.Types.ObjectId, required: true, ref: 'Episode' }],
   name: { type: String, required: true, maxlength: 50 },
   description: { type: String },
 });
--- src/models/series.model.js
+++ src/models/series.model.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 const schema = Schema({ 
-  seasons: { type: Schema.Types.ObjectId, required: true, ref: 'Season' },
+  seasons: [{ type: Schema.Types.ObjectId, required: true, ref: 'Season' }],
   name: { type: String, required: true, maxlength: 50 },
   description: { type: String },
 //   trailer: { type: String, required: true, maxlength: 50 },
